57391a322b010642a170db57a7ec63af1d7778de
vgdn1942 <vgdn1942@gmail.com>
2016-11-13 02:57:45 +0300

    Disabled Ninja

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := A6020,A6020a40,A6020a46,A6020l36,K32c36,k5,k5_plus,vibe_k5
 
+# Ninja
+USE_NINJA := false
+
 # Architecture
 
 TARGET_ARCH := arm64
